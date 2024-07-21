6b429452f0f464197533604976548954b835aeaa
Fauzan Rizky <frizky.erlang@gmail.com>
2024-07-21 13:44:22 +0700

    split up the migrations

--- migrations/1721544015779_create-table-albums.js
+++ migrations/1721544015779_create-table-albums.js
@@ -25,39 +25,6 @@ exports.up = (pgm) => {
       notNull: true,
     },
   });
-  pgm.createTable('songs', {
-    id: {
-      type: 'VARCHAR(50)',
-      primaryKey: true,
-    },
-    title: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    year: {
-      type: 'INTEGER',
-      notNull: true,
-    },
-    genre: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    performer: {
-      type: 'TEXT',
-      notNull: true,
-    },
-    duration: {
-      type: 'INTEGER',
-      notNull: false,
-    },
-    album_id: {
-      type: 'VARCHAR(50)',
-      notNull: false,
-      references: '"albums"',
-      onDelete: 'SET NULL',
-    },
-  });
-  pgm.createIndex('songs', 'album_id');
 };
 
 /**
@@ -66,7 +33,5 @@ exports.up = (pgm) => {
  * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
-  pgm.dropIndex('songs', 'album_id');
-  pgm.dropTable('songs');
   pgm.dropTable('albums');
 };
