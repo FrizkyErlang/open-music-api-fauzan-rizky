033e95f24381a41ab78fed12bd2cd9846fdace24
Fauzan Rizky <frizky.erlang@gmail.com>
2024-09-08 20:28:52 +0700

    move playlist activity to playlist service

--- src/services/postgres/PlaylistService.js
+++ src/services/postgres/PlaylistService.js
@@ -115,6 +115,38 @@ class PlaylistsService {
     }
   }
 
+  async addPlaylistActivity(songId, playlistId, userId, action) {
+    const id = `activity-${nanoid(16)}`;
+    const time = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO playlist_song_activities VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, songId, playlistId, userId, action, time],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Activities gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
+
+  async getPlaylistActivities(playlistId) {
+    const query = {
+      text: 'SELECT * FROM playlist_song_activities WHERE playlist_id = $1',
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Playlist Activity tidak ditemukan');
+    }
+
+    return result.rows;
+  }
+
   async verifyPlaylistOwner(id, owner) {
     const query = {
       text: 'SELECT * FROM playlist WHERE id = $1',
