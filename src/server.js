90dfa921e432e5b2b095d0c3970ba54a0273e413
Fauzan Rizky <frizky.erlang@gmail.com>
2024-09-08 22:18:42 +0700

    finishing server.js

--- src/server.js
+++ src/server.js
@@ -22,12 +22,22 @@ const authentications = require('./api/authentications');
 const AuthenticationsService = require('./services/postgres/AuthenticationService');
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
+// playlist
+const playlist = require('./api/playlists');
+const PlaylistsService = require('./services/postgres/PlaylistService');
+const PlaylistsValidator = require('./validator/playlists');
+// collaborations
+const collaborations = require('./api/collaborations');
+const CollaborationsService = require('./services/postgres/CollaborationService');
+const CollaborationsValidator = require('./validator/collaborations');
 
 const init = async () => {
   const albumService = new AlbumService();
   const songService = new SongService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const collaborationsService = new CollaborationsService();
+  const playlistsService = new PlaylistsService(collaborationsService);
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -96,6 +106,22 @@ const init = async () => {
         usersService,
         tokenManager: TokenManager,
         validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: collaborations,
+      options: {
+        collaborationsService,
+        playlistsService,
+        validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: playlist,
+      options: {
+        collaborationsService,
+        songService,
+        validator: PlaylistsValidator,
       },
     },
   ]);
