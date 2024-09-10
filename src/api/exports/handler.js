3dbb2580f0d27df3214a8bbce4ceba1b772820af
Fauzan Rizky <frizky.erlang@gmail.com>
2024-09-10 22:45:53 +0700

    create exports api

---
+++ src/api/exports/handler.js
@@ -0,0 +1,41 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-underscore-dangle */
+const autoBind = require('auto-bind');
+
+class ExportsHandler {
+  constructor(exportsService, playlistsService, validator) {
+    this._serviceExports = exportsService;
+    this._servicePlaylist = playlistsService;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postExportPlaylistsHandler(request, h) {
+    this._validator.validateExportPlaylistPayload(request.payload);
+    const { id } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._servicePlaylist.verifyPlaylistOwner(id, credentialId);
+
+    const message = {
+      userId: credentialId,
+      targetEmail: request.payload.targetEmail,
+      playlistId: id,
+    };
+
+    await this._serviceExports.sendMessage(
+      'export:playlists',
+      JSON.stringify(message)
+    );
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
